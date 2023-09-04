0c624e89a2c994f823345e0d5caa31122664b441
drag13 <no-questions@github.noreply.com>
2023-09-04 11:17:23 +0300

    1st

--- tools/scaffold.js
+++ tools/scaffold.js
@@ -1,35 +1,37 @@
-const { render } = require("mustache");
-const { join } = require("path");
-const { readFile, writeFile, exists } = require("./fs");
-const { cwd } = require("process");
-const { mkdirSync } = require("fs");
-const course = require("../course.json");
+const { render } = require('mustache');
+const { join } = require('path');
+const { readFile, writeFile, exists } = require('./fs');
+const { cwd } = require('process');
+const { mkdirSync } = require('fs');
+const course = require('../course.json');
 
-const ROOT = "./lessons";
-const LAYOUT = "./templates/_layout.html";
-const BODY = "./templates/_body.html";
+const ROOT = './lessons';
+const LAYOUT = './templates/_layout.html';
+const BODY = './templates/_body.html';
+const TEXT = './templates/_text.md';
 
-function scaffold(layout, body, lesson, i) {
+function scaffold(layout, body, text, lesson, i) {
   const { name, title } = lesson;
   if (!name) {
-    console.error(
-      `\r\nLesson with title ${title} has no name. Scaffolding canceled`
-    );
+    console.error(`\r\nLesson with title ${title} has no name. Scaffolding canceled`);
     return;
   }
 
   const folder = join(cwd(), ROOT, name);
-  const pathToBody = join(folder, "body.html");
-  const pathToLayout = join(folder, "index.html");
+  const pathToBody = join(folder, 'body.html');
+  const pathToLayout = join(folder, 'index.html');
+  const pathToText = join(folder, 'text.md');
 
   lesson.i = i;
 
   const folderExists = exists(folder);
-  const bodyExists = folderExists && exists(pathToBody);
 
   if (!folderExists) {
     mkdirSync(folder);
   }
+
+  const bodyExists = exists(pathToBody);
+  const textExists = exists(pathToText);
 
   // always regenerate index.html for consistency
   const indexHtml = render(layout, lesson);
@@ -39,21 +41,25 @@ function scaffold(layout, body, lesson, i) {
     const bodyHtml = render(body, lesson);
     writeFile(pathToBody, bodyHtml);
   }
+
+  if (!textExists) {
+    const textMd = render(text, lesson);
+    writeFile(pathToText, textMd);
+  }
 }
 
 (function cli([lessonName]) {
   const layout = readFile(LAYOUT);
   const body = readFile(BODY);
+  const text = readFile(TEXT);
 
-  course.lessons = course.lessons.filter(x=>!x.hidden);
+  course.lessons = course.lessons.filter((x) => !x.hidden);
 
   if (lessonName == null) {
     process.stdout.write(`Syncing ALL lectures...`);
-    course.lessons.forEach((lesson, index) =>
-      scaffold(layout, body, lesson, index)
-    );
+    course.lessons.forEach((lesson, index) => scaffold(layout, body, text, lesson, index));
 
-    console.log("DONE");
+    console.log('DONE');
 
     return;
   }
@@ -67,7 +73,7 @@ function scaffold(layout, body, lesson, i) {
   }
 
   process.stdout.write(`Scaffolding lesson ${lesson.name}... `);
-  scaffold(layout, body, lesson, lessonNumber);
+  scaffold(layout, body, text, lesson, lessonNumber);
 
-  console.log("DONE");
+  console.log('DONE');
 })(process.argv.splice(2));
