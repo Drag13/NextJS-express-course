d097d99da645c9556e17dfdca5b3b2e2277a223d
drag13 <no-questions@github.noreply.com>
2024-04-02 09:28:30 +0300

    Add encode tool

---
+++ tools/encode.js
@@ -0,0 +1,11 @@
+const { readFile, writeFile } = require("./fs");
+const { join } = require("path");
+
+(function name(params) {
+  const filePath = params[0] ?? `../sample.tsx`;
+  const fullPath = join(__dirname, filePath);
+  const file = readFile(fullPath);
+  const text = file.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+  const result = `<pre><code class="language-tsx" data-line-numbers>${text}</code></pre>`;
+  writeFile("test.txt", result);
+})(process.argv.splice(2));
